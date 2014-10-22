0e05cba4e79aae96bf9980938849ada0228e2804
Faris Abusada <faris.a@yamsafer.me>
2014-10-22 14:30:26 +0300

    Wrote custom cookie set get function to deal with PATH, angular-cookie does't support that functionality

--- src/angularCurrency/directives/angularCurrencyDirective.js
+++ src/angularCurrency/directives/angularCurrencyDirective.js
@@ -1,5 +1,5 @@
-angular.module('angularCurrency.directives', []).directive('currancySelect', ['$cookieStore', 'CurrencyService',
-	function($cookieStore, CurrencyService) {
+angular.module('angularCurrency.directives', []).directive('currancySelect', ['CurrencyService',
+	function(CurrencyService) {
 		return {
 			restrict: 'A',
 			link: function(scope, iElement, iAttrs) {},
@@ -13,14 +13,13 @@ angular.module('angularCurrency.directives', []).directive('currancySelect', ['$
 				$scope.setCurrency = function(currency) {
 					CurrencyService.fetchRates(currency.code).then(function(rate) {
 						$scope.selectedCurrency = $scope.list[rate.code];
-						$cookieStore.put('currencyCode', currency.code);
+						setCookie('currencyCode', currency.code);
 					});
 				}
 
 				// Check if cookie has any value
-				var codeFromCookie = $cookieStore.get('currencyCode');
+				var codeFromCookie = getCookie('currencyCode');
 				if (codeFromCookie) {
-
 					$scope.selectedCurrency = $scope.list[codeFromCookie];
 					console.log($scope.selectedCurrency);
 					$scope.setCurrency($scope.selectedCurrency);
@@ -28,10 +27,30 @@ angular.module('angularCurrency.directives', []).directive('currancySelect', ['$
 					// Get default value
 					CurrencyService.fetchRates().then(function(rate) {
 						$scope.selectedCurrency = $scope.list[rate.code];
-						$cookieStore.put('currencyCode', rate.code);
+						setCookie('currencyCode', rate.code);
 					});
 				}
 
+
+
+				function getCookie(cname) {
+					var name = cname + "=";
+					var ca = document.cookie.split(';');
+					for (var i = 0; i < ca.length; i++) {
+						var c = ca[i];
+						while (c.charAt(0) == ' ') c = c.substring(1);
+						if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+					}
+					return "";
+				}
+
+				function setCookie(cname, cvalue, exdays) {
+					var d = new Date();
+					d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+					var expires = "expires=" + d.toUTCString();
+					document.cookie = cname + "=" + cvalue + "; " + expires + ";path=/"
+					console.log(document.cookie);
+				}
 
 
 			},
